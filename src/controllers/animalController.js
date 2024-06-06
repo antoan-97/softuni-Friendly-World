7358ac81c46e2264ec34fcf2347e51019b8222a4
antoan-97 <antoan.todorov@abv.bg>
2024-06-06 14:16:39 +0300

    add create post router

--- src/controllers/animalController.js
+++ src/controllers/animalController.js
@@ -1,4 +1,31 @@
 const router = require('express').Router();
+const animalManager = require('../managers/animalManager');
+const { getErrorMessage } = require('../utils/errorHelper')
+
+
+router.get('/create', async (req, res) => {
+    try {
+        res.render('create');
+    } catch (err) {
+        res.render('404')
+    }
+
+});
+
+
+router.post('/create', async (req, res) => {
+    const animalData = {
+        ...req.body,
+        owner: req.user._id,
+    }
+
+    try {
+        await animalManager.create(animalData);
+        res.redirect('/')
+    } catch (err) {
+        res.render('404', { error: getErrorMessage(err) })
+    }
+});
 
 
 module.exports = router;
