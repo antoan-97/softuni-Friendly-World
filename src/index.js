04a5530756bd9e953375e3aea3f3a4e40cb5b968
antoan-97 <antoan.todorov@abv.bg>
2024-06-06 15:52:46 +0300

    create search.hvs

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ const { auth } = require('./middlewares/authMiddleware');
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/friendly-world')
-.then(() => console.log('DB Connected'))
-.catch(err => console.log('DB Error,', err.message));
+    .then(() => console.log('DB Connected'))
+    .catch(err => console.log('DB Error,', err.message));
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
@@ -25,7 +25,7 @@ app.set('views', 'src/views');
 
 
 app.use(express.static(path.resolve(__dirname, 'public')));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(auth);
 app.use(routes);
