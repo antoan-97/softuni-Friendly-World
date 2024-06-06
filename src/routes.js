343f9322026d95b855517421a3371b32ea56d193
antoan-97 <antoan.todorov@abv.bg>
2024-06-06 14:16:09 +0300

    add animalController in routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const userController = require('./controllers/userController');
+const animalController = require('./controllers/animalController');
 
 
 
 router.use(homeController);
 router.use('/users',userController);
+router.use('/animals', animalController);
 
 
 module.exports = router
