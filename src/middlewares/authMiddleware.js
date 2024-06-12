d7c8515dd2387abd2c3acda7deada3a93d5ba2d1
antoan-97 <antoan.todorov@abv.bg>
2024-06-12 14:34:16 +0300

    create login and owner middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,6 @@
 const jwt = require('../lib/jwt');
 const { SECRET } = require('../config/config');
+const animalManager = require('../managers/animalManager');
 
 
 exports.auth = async (req,res,next) => {
@@ -25,6 +26,24 @@ exports.auth = async (req,res,next) => {
 exports.isAuth = (req,res,next) =>{
     if(!req.user){
         res.redirect('/users/login');
+    }
+    next();
+};
+
+exports.isLoggedIn = (req, res, next) => {
+    if (req.user) {
+        // Redirect to 404 page  if user is already logged in
+        return res.render('404');
+    }
+    next();
+};
+
+
+exports.isOwner = async (req, res, next) => {
+    const animalId = req.params.animalId;
+    const animal = await animalManager.getOne(animalId);
+    if (!animal || animal.owner.toString() !== req.user._id.toString()) {
+        return res.render('404');
     }
     next();
 };
